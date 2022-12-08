e04a74d2d824fe384fc3737b798bbff65ec1d9c4
Dmitry Alexeev <Alekseyevdv1981@yandex.ru>
2022-12-08 21:08:52 +0300

    Task final with mistakes

--- task_final/solution.js
+++ task_final/solution.js
@@ -32,9 +32,10 @@
 //      ]
 //    }
 // }
+let address = {street: "street", house: "house", entrance: "entrance", floor: "floor", flat: "flat"};
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: `${name} + ${phone}`, goods: [title, count], order: {address: address, sum: sum}};
 
     let countOfGoods = goods.length;
 
@@ -43,11 +44,12 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name;
 
     let jsonData = JSON.stringify(data);
 
     return jsonData;
+}
 }
